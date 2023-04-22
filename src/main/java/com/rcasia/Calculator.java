0a14000cb3ea0f87d6e53cc2f8dbceee66b5ad9c
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-04-22 12:30:35 +0200

    Calculator subtracts

--- src/main/java/com/rcasia/Calculator.java
+++ src/main/java/com/rcasia/Calculator.java
@@ -4,12 +4,17 @@ public class Calculator {
     
     private int current = 0;
     
+    public int getCurrent() {
+        return this.current;
+    }
+    
     public Calculator sum(int a) {
         this.current +=  a;
         return this;
     }
 
-    public int getCurrent() {
-        return this.current;
+    public Calculator substract(int b) {
+        this.current -= b;
+        return this;
     }
 }
--- src/test/java/com/rcasia/CalculatorTest.java
+++ src/test/java/com/rcasia/CalculatorTest.java
@@ -34,6 +34,25 @@ class CalculatorTest {
         
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+    
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,0",
+            "2,1,1",
+            "9,3,6",
+            "100,23,77"
+    })
+    void subtractAMinusBEqualsC(int a, int b, int expected){
+        // when
+        this.calculator
+                .sum(a)
+                .substract(b);
+        
+        int actual = this.calculator.getCurrent();
+        
+        // then
+        assertThat(actual).isEqualTo(expected);
         
     }
 }
